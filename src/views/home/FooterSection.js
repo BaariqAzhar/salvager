f55cd51cfe5c14f4b9f50c76867cf36db978099c
BaariqAzhar <36961071+BaariqAzhar@users.noreply.github.com>
2020-04-03 21:37:32 +0700

    add container in footer

--- src/views/home/FooterSection.js
+++ src/views/home/FooterSection.js
@@ -8,7 +8,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
-import { Grid } from '@material-ui/core';
+import { Grid, Container } from '@material-ui/core';
 import Box from '@material-ui/core/Box';
 // import MenuIcon from '@material-ui/icons/Menu';
 import WhiteLogo from '../../assets/img/white-logo.png';
@@ -29,97 +29,100 @@ export default function FooterSection() {
     const classes = useStyles();
 
     return (
-        <div>
-            <Grid container className={classes.container}>
-                <Grid item xs={12} sm={4} className={classes.gridItem}>
-                    <Box
-                        display="flex"
-                        alignItems="center"
-                        p={1}
-                        m={1}
-                        css={{ height: 100 }}
-                    >
-                        <img align="center"
-                            justify="center"
-                            direction="column" src={WhiteLogo} className={classes.whiteLogo} />
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={3} className={classes.gridItem}>
-                    <Typography variant="subtitle1" gutterBottom className={classes.text}>
-                        Official Account
+        <div className={classes.firstDiv}>
+            <Container>
+                <Grid container>
+                    <Grid item xs={12} sm={4} className={classes.gridItem}>
+                        {/* <img src={WhiteLogo} className={classes.whiteLogo} /> */}
+                        <Box
+                            display="flex"
+                            alignItems="center"
+                            p={1}
+                            m={1}
+                            css={{ height: 100 }}
+                        >
+                            <img align="center"
+                                justify="center"
+                                direction="column" src={WhiteLogo} className={classes.whiteLogo} />
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} sm={3} className={classes.gridItem}>
+                        <Typography variant="subtitle1" gutterBottom className={classes.text}>
+                            Official Account
                     </Typography>
-                    <div className={classes.listSocialMedia}>
-                        <img src={WaIcon} className={classes.icon} />
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            +628123456789
+                        <div className={classes.listSocialMedia}>
+                            <img src={WaIcon} className={classes.icon} />
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                +628123456789
                         </Typography>
-                    </div>
-                    <div className={classes.listSocialMedia}>
-                        <img src={TokpedIcon} className={classes.icon} />
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            salvager
+                        </div>
+                        <div className={classes.listSocialMedia}>
+                            <img src={TokpedIcon} className={classes.icon} />
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                salvager
                         </Typography>
-                    </div>
-                    <div className={classes.listSocialMedia}>
-                        <img src={IgIcon} className={classes.icon} />
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            salvagerindonesia
+                        </div>
+                        <div className={classes.listSocialMedia}>
+                            <img src={IgIcon} className={classes.icon} />
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                salvagerindonesia
                         </Typography>
-                    </div>
-                </Grid>
-                <Grid item xs={12} sm={2} className={classes.gridItem}>
-                    <Typography variant="subtitle1" gutterBottom className={classes.text}>
-                        Info
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={2} className={classes.gridItem}>
+                        <Typography variant="subtitle1" gutterBottom className={classes.text}>
+                            Info
                     </Typography>
-                    <div className={classes.listSocialMedia}>
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            Payment Info
+                        <div className={classes.listSocialMedia}>
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                Payment Info
                         </Typography>
-                    </div>
-                    <div className={classes.listSocialMedia}>
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            FAQ
+                        </div>
+                        <div className={classes.listSocialMedia}>
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                FAQ
                         </Typography>
-                    </div>
-                    <div className={classes.listSocialMedia}>
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            Term &amp; Condition
+                        </div>
+                        <div className={classes.listSocialMedia}>
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                Term &amp; Condition
                         </Typography>
-                    </div>
-                    <div className={classes.listSocialMedia}>
-                        <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
-                            Contact
+                        </div>
+                        <div className={classes.listSocialMedia}>
+                            <Typography display="inline" variant="subtitle2" gutterBottom className={classes.text}>
+                                Contact
                         </Typography>
-                    </div>
+                        </div>
+                    </Grid>
+                    <Grid item xs={12} sm={3} className={classes.gridItem}>
+                        <div>
+                            <img src={letterIcon} className={classes.letterIcon} />
+                            <Typography display="inline" variant="subtitle1" gutterBottom className={classes.text}>
+                                Join our mailing list
+                        </Typography>
+                        </div>
+                        <form >
+                            <TextField className={classes.letter} placeholder="your email" />
+                            <br />
+                            <Button className={classes.button} variant="contained">
+                                Send </Button>
+                        </form>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={3} className={classes.gridItem}>
-                    <div>
-                        <img src={letterIcon} className={classes.letterIcon} />
-                        <Typography display="inline" variant="subtitle1" gutterBottom className={classes.text}>
-                            Join our mailing list
-                        </Typography>
-                    </div>
-                    <form >
-                        <TextField className={classes.letter} placeholder="your email" />
-                        <br/>
-                        <Button className={classes.button} variant="contained">
-                            Send </Button>
-                    </form>
-                </Grid>
-            </Grid>
+            </Container>
         </div >
     )
 }
 
 const useStyles = makeStyles(theme => ({
-    container: {
+    firstDiv: {
         backgroundColor: '#434c57',
         marginTop: '3rem',
         padding: '2rem',
         color: 'white'
     },
     whiteLogo: {
-        width: '90%',
+        width: '80%',
         marginTop: 'auto',
         marginBottom: 'auto'
     },
